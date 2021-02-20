5f7756718489c0bb9396daedeab5c4b34c35a9d8
Martin Kunz <martinkunz@email.cz>
2021-02-20 22:27:24 +0100

    format: change long string delims to [[]]

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -8,7 +8,7 @@ g.maplocalleader = '-' -- leader local to buffer (for buffer local mappings)
 
 --  NORMAL  --
 map('n', '<CR>',
-    ('vim.fn.append(vim.fn.line("."), vim.fn["repeat"]({""}, vim.api.nvim_get_vvar("count1")))'):is_cmd{
+    ([[vim.fn.append(vim.fn.line("."), vim.fn["repeat"]({""}, vim.api.nvim_get_vvar("count1")))]]):is_cmd{
   lua = true,
 }, {silent = true}) -- N<CR> to add N lines below cursor
 map('n', 'Y', 'y$', {noremap = true}) -- Y to act as D and C
@@ -19,6 +19,6 @@ map('v', '<<', '<<<Esc>gv', {noremap = true}) -- reselect after <<
 
 -- COMMAND--
 map('c', 'w!!',
-    ('vim.fn.execute("silent! write !sudo tee % >/dev/null") <bar> edit!'):is_cmd{
+    ([[vim.fn.execute("silent! write !sudo tee % >/dev/null") <bar> edit!]]):is_cmd{
   lua = true,
 }, {noremap = true}) -- writing read-only files
