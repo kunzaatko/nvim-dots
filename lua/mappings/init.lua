b839dc9aa1c9c59d3b6e946804b095aba0b1ce12
Martin Kunz <martinkunz@email.cz>
2021-02-20 22:19:57 +0100

    fix: use of local variables in mappings
    Even though, I exported the api, the full api paths need to be used for
    mappings, since they are not run withing the file.

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -8,7 +8,7 @@ g.maplocalleader = '-' -- leader local to buffer (for buffer local mappings)
 
 --  NORMAL  --
 map('n', '<CR>',
-    ('fn.append(fn.line("."), fn["repeat"]({""}, api.nvim_get_vvar("count1")))'):is_cmd{
+    ('vim.fn.append(vim.fn.line("."), vim.fn["repeat"]({""}, vim.api.nvim_get_vvar("count1")))'):is_cmd{
   lua = true,
 }, {silent = true}) -- N<CR> to add N lines below cursor
 map('n', 'Y', 'y$', {noremap = true}) -- Y to act as D and C
@@ -18,5 +18,7 @@ map('v', '>>', '>><Esc>gv', {noremap = true}) -- reselect after >>
 map('v', '<<', '<<<Esc>gv', {noremap = true}) -- reselect after <<
 
 -- COMMAND--
-map('c', 'w!!', 'vim.fn.execute("silent! write !sudo tee % >/dev/null") <bar> edit!',
-    {noremap = true}) -- writing read-only files
+map('c', 'w!!',
+    ('vim.fn.execute("silent! write !sudo tee % >/dev/null") <bar> edit!'):is_cmd{
+  lua = true,
+}, {noremap = true}) -- writing read-only files
