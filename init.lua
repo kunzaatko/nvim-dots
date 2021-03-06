b4061e7feb6bdad6ad732fabeb8b174a4fb83ba2
Martin Kunz <martinkunz@email.cz>
2021-03-06 21:02:57 +0100

    Edit: opting out of loading default packages

--- init.lua
+++ init.lua
@@ -2,6 +2,13 @@
 vim.g.loaded_python_provider = 0
 vim.g.python3_host_prog = '/usr/bin/python3'
 
+-- Disable some built-in plugins
+local disabled_built_ins = {
+    'gzip', 'man', 'matchit', 'matchparen', 'shada_plugin', 'tarPlugin', 'tar',
+    'zipPlugin', 'zip', 'netrwPlugin'
+}
+for _, plugin in ipairs(disabled_built_ins) do vim.g['loaded_' .. plugin] = 1 end
+
 --  Options  --
 require 'options'
 
