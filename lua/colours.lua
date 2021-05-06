b8d198df35151927e5145ed2f25441930b8cfaaf
Martin Kunz <martinkunz@email.cz>
2021-05-06 16:35:55 +0200

    add: Clear highlighting for FoldMarker

--- lua/colours.lua
+++ lua/colours.lua
@@ -1,2 +1,6 @@
 local autocmd = require'utils'.autocmd
 autocmd('','TextYankPost * silent! lua vim.highlight.on_yank{higroup="Search", timeout=300, on_visual=false, on_macro=false}')
+
+-- fold marker highlighting
+vim.cmd 'highlight default link FoldMarker vimHiClear'
+
