4bc4b1bfdc894564acba26d796d150d3ead8b073
Martin Kunz <martinkunz@email.cz>
2021-05-10 12:59:22 +0200

    edit: do not use utils map

--- lua/conf/pkgs/comfortable-motion.lua
+++ lua/conf/pkgs/comfortable-motion.lua
@@ -1,7 +1,9 @@
-local map = require'utils'.map
+local map = vim.api.nvim_set_keymap
 
 vim.g.comfortable_motion_friction = 70.0
 vim.g.comfortable_motion_air_drag = 3.0
 
-map('n','<ScrollWheelUp>', [[<Cmd> lua vim.call'comfortable_motion#flick(-20)<CR>']],{silent = true})
-map('n','<ScrollWheelDown>', [[<Cmd> lua vim.call'comfortable_motion#flick(20)<CR>']],{silent = true})
+map('n', '<ScrollWheelUp>', [[<Cmd> lua vim.call'comfortable_motion#flick(-20)<CR>']],
+    {silent = true})
+map('n', '<ScrollWheelDown>', [[<Cmd> lua vim.call'comfortable_motion#flick(20)<CR>']],
+    {silent = true})
