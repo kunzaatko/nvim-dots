c71d0d55aae8658ce441fb5bd87fecb91a861c69
Martin Kunz <martinkunz@email.cz>
2021-05-19 22:41:41 +0200

    add: snippets to repository and change loading directory for ultisnips

--- lua/conf/pkgs/ultisnips.lua
+++ lua/conf/pkgs/ultisnips.lua
@@ -3,6 +3,6 @@ vim.g.snips_author = "kunzaatko"
 vim.g.snips_email = "martinkunz@email.cz"
 vim.g.snips_github = "https://github.com/kunzaatko"
 -- UltiSnips
-vim.g.UltiSnipsSnippetDirectories = {'/home/kunzaatko/.local/share/nvim/snips'} -- this ensures that snippets directories are not recursively searched
+vim.g.UltiSnipsSnippetDirectories = {'/home/kunzaatko/.config/nvim/snips'} -- this ensures that snippets directories are not recursively searched
 vim.g.UltiSnipsEnableSnipMate = 0 -- only look for UltiSnips snippets
 vim.g.UltiSnipsExpandTrigger = '<c-l>' -- overwrites escaping to insert mode when insertmode option is set
