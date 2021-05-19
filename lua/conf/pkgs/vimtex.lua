ce9d19522a0cfc5962d25ef5f07e8b1ba427679c
Martin Kunz <martinkunz@email.cz>
2021-05-19 21:07:17 +0200

    add: -shell-escape flag for latexmk for minted package

--- lua/conf/pkgs/vimtex.lua
+++ lua/conf/pkgs/vimtex.lua
@@ -13,3 +13,17 @@ vim.g.vimtex_fold_manual = 1 -- only compute only on demand
 vim.g.vimtex_view_use_temp_files = 2 -- ensures, that the PDF does not update after a failed compilation
 vim.g.vimtex_view_automatic = 1 -- for mklatex it has to be set only if the use_temp_files is nonzero
 vim.g.vimtex_view_method = 'zathura'
+vim.g.vimtex_compiler_latexmk = {
+        build_dir = '',
+        callback = 1,
+         continuous = 1,
+         executable = 'latexmk',
+         hooks = {},
+         options = {
+           '-verbose',
+           '-file-line-error',
+           '-synctex=1',
+           '-interaction=nonstopmode',
+           '-shell-escape' -- for running python for the minted package to work
+         }
+        }
