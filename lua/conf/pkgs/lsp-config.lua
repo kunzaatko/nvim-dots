e2cc0ef7e702a3697ea08b516d16616c583d523d
Martin Kunz <martinkunz@email.cz>
2021-07-23 11:05:37 +0200

    edit: on_attach function definition of mappings§

--- lua/conf/pkgs/lsp-config.lua
+++ lua/conf/pkgs/lsp-config.lua
@@ -1,24 +1,52 @@
-local lsp = require 'lspconfig'
-local attach_mappings = function()
-  require "map.pkgs.lsp-mappings"
+local lspconfig = require 'lspconfig'
+local on_attach = function(_, bufnr)
+  -- {{{
+  local map_loc = vim.api.nvim_buf_set_keymap
+
+  map_loc(bufnr, 'n', '<c-]>', "<Cmd>lua vim.lsp.buf.definition()<CR>",
+          {noremap = true, silent = true})
+  map_loc(bufnr, 'n', 'K', "<Cmd>lua vim.lsp.buf.hover()<CR>",
+          {noremap = true, silent = true})
+  map_loc(bufnr, 'n', 'gD', "<Cmd>lua vim.lsp.buf.implementation()<CR>",
+          {noremap = true, silent = true})
+  map_loc(bufnr, 'n', '<c-k>', "<Cmd>lua vim.lsp.buf.signature_help()<CR>",
+          {noremap = true, silent = true})
+  map_loc(bufnr, 'n', '1gD', "<Cmd>lua vim.lsp.buf.type_definition()<CR>",
+          {noremap = true, silent = true})
+  map_loc(bufnr, 'n', 'gr', "<Cmd>lua vim.lsp.buf.references()<CR>",
+          {noremap = true, silent = true})
+  map_loc(bufnr, 'n', 'g0', "<Cmd>lua vim.lsp.buf.document_symbol()<CR>",
+          {noremap = true, silent = true})
+  map_loc(bufnr, 'n', 'gW', "<Cmd>lua vim.lsp.buf.workspace_symbol()<CR>",
+          {noremap = true, silent = true})
+  map_loc(bufnr, 'n', 'gd', "<Cmd>lua vim.lsp.buf.declaration()<CR>",
+          {noremap = true, silent = true})
+  map_loc(bufnr, 'n', 'ga', "<Cmd>lua vim.lsp.buf.code_action()<CR>",
+          {noremap = true, silent = true})
+
+  -- Goto previous/next diagnostic warning/error
+  map_loc(bufnr, 'n', 'g[', "<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>",
+          {noremap = true, silent = true})
+  map_loc(bufnr, 'n', 'g]', "<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>",
+          {noremap = true, silent = true})
+end -- }}}
+
+local servers = {"bashls", "texlab", "ccls", "rust_analyzer", "vimls"}
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
 end
 
-lsp.bashls.setup {on_attach = attach_mappings()}
-
-lsp.texlab.setup {on_attach = attach_mappings()}
-
-lsp.ccls.setup {on_attach = attach_mappings()}
-
-lsp.rust_analyzer.setup {on_attach = attach_mappings()}
-
-lsp.vimls.setup {on_attach = attach_mappings()}
-
-lsp.julials.setup {
+lspconfig.julials.setup {
   settings = {julia = {format = {indent = 2}}},
-  on_attach = attach_mappings(),
-  on_new_config = function(new_config, new_root_dir)
-    server_path = "/home/kunzaatko/.julia/packages/LanguageServer/y1ebo/src"
-    cmd = {
+  on_attach = on_attach,
+  on_new_config = function(new_config, _)
+    local server_path = "/home/kunzaatko/.julia/packages/LanguageServer/y1ebo/src"
+    local cmd = {
       "julia",
       "--project=" .. server_path,
       "--startup-file=no",
@@ -41,13 +69,13 @@ lsp.julials.setup {
   end,
 }
 
-lsp.pyright.setup {
+lspconfig.pyright.setup {
   settings = {python = {formatting = {provider = 'yapf'}}},
-  on_attach = attach_mappings(),
+  on_attach = on_attach,
 }
 
-lsp.sumneko_lua.setup {
-  on_attach = attach_mappings(),
+lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
   cmd = {'lua-language-server'},
   settings = {
     Lua = {
