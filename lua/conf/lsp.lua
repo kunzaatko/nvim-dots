d2f1a5f5d8af6454e9f7f95c50d6833ac518f4b8
Martin Kunz <martinkunz@email.cz>
2021-07-25 03:09:10 +0200

    edit: do not show diagnostics as virtual text

--- lua/conf/lsp.lua
+++ lua/conf/lsp.lua
@@ -6,7 +6,7 @@ vim.opt.updatetime = 300
 -- Enable diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
   vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
-               {virtual_text = true, signs = true, update_in_insert = true})
+               {virtual_text = false, signs = true, update_in_insert = true})
 
 cmd [[
     augroup ShowDiagnosticsPopup
