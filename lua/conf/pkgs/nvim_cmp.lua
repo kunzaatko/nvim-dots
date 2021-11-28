7f082ae938384bb341d5000c0cc58cc0dc36e252
Martin Kunz <martinkunz@email.cz>
2021-11-29 00:50:33 +0100

    edit: lsp-config for julials can be default

--- lua/conf/pkgs/nvim_cmp.lua
+++ lua/conf/pkgs/nvim_cmp.lua
@@ -16,29 +16,6 @@ end
 lspconfig.julials.setup {
   settings = {julia = {format = {indent = 2}}},
   on_attach = LSPUtils.on_attach,
-  on_new_config = function(new_config, _)
-    local server_path = "/home/kunzaatko/.julia/packages/LanguageServer/y1ebo/src"
-    local cmd = {
-      "julia",
-      "--project=" .. server_path,
-      "--startup-file=no",
-      "--history-file=no",
-      "-e",
-      [[
-          using Pkg;
-          Pkg.instantiate()
-          using LanguageServer; using SymbolServer;
-          depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
-          project_path = dirname(something(Base.current_project(pwd()), Base.load_path_expand(LOAD_PATH[2])))
-          # Make sure that we only load packages from this environment specifically.
-          @info "Running language server" env=Base.load_path()[1] pwd() project_path depot_path
-          server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path);
-          server.runlinter = true;
-          run(server);
-        ]],
-    };
-    new_config.cmd = cmd
-  end,
   capabilities = capabilities,
 }
 
