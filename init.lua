f730d23871116bf3b26121a2e82c17ac7c63d612
Martin Kunz <martinkunz@email.cz>
2021-12-07 15:24:27 +0100

    edit: format

--- init.lua
+++ init.lua
@@ -4,10 +4,20 @@ vim.g.python3_host_prog = '/usr/bin/python3'
 
 -- Disable built-in plugins
 local disabled_built_ins = {
-    'gzip', 'man', 'matchit', 'matchparen', 'shada_plugin', 'tarPlugin', 'tar',
-    'zipPlugin', 'zip', 'netrwPlugin'
+  'gzip',
+  'man',
+  'matchit',
+  'matchparen',
+  'shada_plugin',
+  'tarPlugin',
+  'tar',
+  'zipPlugin',
+  'zip',
+  'netrwPlugin',
 }
-for _, plugin in ipairs(disabled_built_ins) do vim.g['loaded_' .. plugin] = 1 end
+for _, plugin in ipairs(disabled_built_ins) do
+  vim.g['loaded_' .. plugin] = 1
+end
 
 --  config  --
 require 'conf'
