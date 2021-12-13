14ea1c9dd48218c2ffe84a5f042429b0363cea7a
Martin Kunz <martinkunz@email.cz>
2021-12-13 16:01:39 +0100

    add(pkg): terminal toggle with persisting session

--- lua/pkg/init.lua
+++ lua/pkg/init.lua
@@ -787,6 +787,37 @@ return packer.startup {
     -- }}}
 
     -- }}}
+
+    -- TERMINAL {{{
+    -- akinsho/toggleterm.nvim -- toggle the terminal but persist the session{{{
+    use {
+      'akinsho/toggleterm.nvim',
+      keys = {'<C-p>'},
+      config = function()
+        vim.cmd [[autocmd! TermOpen term://* lua vim.opt_local.spell = false]]
+        require'toggleterm'.setup {
+          size = function(term)
+            if term.direction == "horizontal" then
+              return 15
+            elseif term.direction == "vertical" then
+              return vim.o.columns * 0.4
+            end
+          end,
+          open_mapping = '<C-p>',
+          hide_numbers = true, -- hide the number column in toggleterm buffers
+          shade_filetypes = {},
+          shade_terminals = true,
+          shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+          start_in_insert = true,
+          insert_mappings = true, -- whether or not the open mapping applies in insert mode
+          persist_size = true,
+          direction = 'vertical',
+          close_on_exit = true, -- close the terminal window when the process exits
+          shell = vim.o.shell, -- change the default shell
+        }
+      end,
+    }
+    -- }}}
     -- }}}
 
     -- LIBRARIES {{{
