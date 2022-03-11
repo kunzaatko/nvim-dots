f5c120cd13bb3ad7e238e9e4c311744bccc15b70
Martin Kunz <martinkunz@email.cz>
2022-03-11 12:09:42 +0100

    add(pkg): util function for adding keys in packer
    Helper function for the packer spec for adding keys for conditional
    loading.

--- lua/pkg/utils.lua
+++ lua/pkg/utils.lua
@@ -23,4 +23,55 @@ M.packer_install = function()
   end
 end
 
+--- Create a table of strings as a product of the strings in the table
+---@param keys table[string]
+---@return table[string]
+local function string_product(keys)
+  if #keys == 1 then
+    return keys[1]
+  end
+  local keys_copy = {}
+  for _, key in ipairs(keys) do
+    table.insert(keys_copy, key)
+  end
+  local product = {}
+  local first = table.remove(keys_copy, 1)
+  for _, a in ipairs(first) do
+    if type(a) ~= 'string' then
+      vim.cmd 'echoerr "Not a valid keys table"'
+    end
+    for _, b in ipairs(string_product(keys_copy)) do
+      table.insert(product, a .. b)
+    end
+  end
+  return product
+end
+
+local function expand_keys(keys)
+  if type(keys) == 'string' then
+    return { keys }
+  elseif type(keys[1]) == 'string' then
+    return keys
+  else
+    return string_product(keys)
+  end
+end
+
+--- Create the mappings table for packer from the modes and as a product of strings in keys
+---@param modes table[string]|string modes to to create the mappings in
+---@param keys table[string]|string keys to make the mappings from
+---@return table[table[string]] spec of mappings for packer
+M.get_keys = function(modes, keys)
+  if type(modes) == 'string' then
+    modes = { modes }
+  end
+  local combinations = {}
+  for _, mode in ipairs(modes) do
+    for _, v in ipairs(expand_keys(keys)) do
+      table.insert(combinations, { mode, v })
+    end
+  end
+  return combinations
+end
+
 return M
