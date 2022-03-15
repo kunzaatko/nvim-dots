d77418dce5fd7b25e23b0e71ca034d2751e65c56
Martin Kunz <martinkunz@email.cz>
2022-03-15 22:16:09 +0100

    fix(map): in ftplugin/tex.lua was mapping a function output instead of pointer

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -4,13 +4,10 @@ require 'utils.texutils'
 vim.opt.conceallevel = 1
 vim.g.tex_conceal = 'abdmg' -- no s... do not conceal sub/sup scripts
 
-map(
-  'i',
-  '<C-f>',
-  _G.MUtils.inkscape_figures_create(vim.api.nvim_get_current_line()),
-  { expr = true, silent = true, desc = 'create an incscape figure' }
-)
-map('n', '<C-f>', _G.MUtils.inkscape_figures_edit(), { expr = true, silent = true, desc = 'edit an incscape figure' })
+map('i', '<C-f>', function()
+  _G.MUtils.inkscape_figures_create(vim.api.nvim_get_current_line())
+end, { expr = true, silent = true, desc = 'create an incscape figure' })
+map('n', '<C-f>', _G.MUtils.inkscape_figures_edit, { expr = true, silent = true, desc = 'edit an incscape figure' })
 
 -- TODO: On v0.7 change to lua API autocommand <10-03-22, kunzaatko> --
 vim.cmd [[
