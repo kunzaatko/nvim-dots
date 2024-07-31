3a8e369f6d125971da83c24e9bdf45588529f171
Martin Kunz <martinkunz@email.cz>
2024-07-31 10:12:13 +0200

    feat: Keep cursor position after yank
    Vim nano tips recommendation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -110,8 +110,25 @@ end, { desc = 'Toggle spell' })
 --   vim.opt.spell = not vim.opt.spell:get()
 -- end, { desc = 'Toggle spell global' })
 
--- changing default behaviour
-vim.keymap.set('n', 'Y', 'y$', { desc = 'yank to eol' }) -- Y to act as D and C
+local cursorPreYank
+vim.keymap.set({ 'n', 'x' }, 'y', function()
+  cursorPreYank = vim.api.nvim_win_get_cursor(0)
+  return 'y'
+end, { expr = true })
+
+vim.keymap.set('n', 'Y', function()
+  cursorPreYank = vim.api.nvim_win_get_cursor(0)
+  return 'y$'
+end, { expr = true, desc = 'yank to eol' }) -- Y to act as D and C
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    if vim.v.event.operator == 'y' and cursorPreYank then
+      vim.api.nvim_win_set_cursor(0, cursorPreYank)
+    end
+  end,
+})
+
 -- workaround for spellcheck
 -- wk.add {
 --   {
