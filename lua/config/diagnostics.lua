afe31d5d83c0ba2bc4d578aa90ea00818a9d0389
Martin Kunz <martinkunz@email.cz>
2024-08-07 23:43:39 +0200

    fix(deprecated): `sign_define` depracated by `Opts.Signs`

--- lua/config/diagnostics.lua
+++ lua/config/diagnostics.lua
@@ -1,9 +1,21 @@
-for name, icon in pairs(static.icons.diagnostics) do
-  name = 'DiagnosticSign' .. name
-  vim.fn.sign_define(name, { text = icon, texthl = name, numhl = '' })
+local severity_names = {
+  Error = vim.diagnostic.severity.ERROR,
+  Warn = vim.diagnostic.severity.WARN,
+  Info = vim.diagnostic.severity.INFO,
+  Hint = vim.diagnostic.severity.HINT,
+}
+local sign_opts = {
+  text = {},
+  numhl = {},
+}
+for name, sname in pairs(severity_names) do
+  local hl_name = 'DiagnosticSign' .. name
+  sign_opts.numhl[sname] = hl_name
+  sign_opts.text[sname] = static.icons.diagnostics[name]
 end
 
 vim.diagnostic.config {
+  signs = sign_opts,
   underline = true,
   update_in_insert = false,
   virtual_text = false, -- set in lua/plugins/lsp/init.lua with lsp_lines
