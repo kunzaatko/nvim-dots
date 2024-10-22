32c143bbc4cb2293d5ded0d5caa10cf13bc177e3
Martin Kunz <martinkunz@email.cz>
2024-10-22 11:17:15 +0200

    fix(Telescope): No valid Tree-Sitter parser for PDF

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -39,7 +39,7 @@ return {
         },
         preview = {
           treesitter = {
-            disable = { 'text' },
+            disable = { 'text', 'pdf' },
           },
         },
         mappings = {
