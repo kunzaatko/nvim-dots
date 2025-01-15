36e380b3f41c5ba65c76d6e45377f00d3b266f33
Martin Kunz <martinkunz@email.cz>
2025-01-15 11:29:40 +0100

    feat(plugins): Replace `headlines.nvim` by `render-markdown.nvim`
    Plugins that are for rendering markdown by concealment.

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -15,18 +15,13 @@ return {
     end,
   },
   {
-    'lukas-reineke/headlines.nvim',
-    -- TODO: Consider adding other filetypes <18-04-23>
-    ft = 'markdown',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', name = 'treesitter' },
-    opts = {},
-    config = function()
-      require('headlines').setup()
-      -- TODO: Change the highlights <18-04-23>
-      vim.cmd [[highlight Headline1 guibg=#1e2718]]
-      vim.cmd [[highlight Headline2 guibg=#21262d]]
-      vim.cmd [[highlight CodeBlock guibg=#1c1c1c]]
-      vim.cmd [[highlight Dash guibg=#D19A66 gui=bold]]
-    end,
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ft = { 'markdown' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    -- opts = {},
+    -- TODO: Configure <14-12-24>
+    config = true,
   },
 }
