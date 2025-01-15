2edaa1c78394a93767202515210efc0d55560eab
Martin Kunz <martinkunz@email.cz>
2025-01-15 11:39:41 +0100

    add(plugins): `hardtime.nvim`
    For changing habit of `hjkl` spamming and to support better movement
    habits.

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -339,6 +339,14 @@ return {
     },
   },
   {
+    'm4xshen/hardtime.nvim',
+    lazy = false,
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {
+      disable_mouse = false,
+    },
+  },
+  {
     'godlygeek/tabular',
     event = 'VeryLazy',
   },
