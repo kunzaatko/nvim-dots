7dc35f2f40c8ad0cf9a622f2f3ea401c121787e0
Martin Kunz <martinkunz@email.cz>
2025-02-26 14:08:59 +0100

    add(plugin): yazi.nvim

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,22 @@
+return {
+  'mikavilpas/yazi.nvim',
+  event = 'VeryLazy',
+  keys = {
+    {
+      '<leader>-',
+      mode = { 'n', 'v' },
+      '<cmd>Yazi<cr>',
+      desc = 'Open yazi at the current file',
+    },
+    {
+      '<leader>cw',
+      '<cmd>Yazi cwd<cr>',
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      '<c-up>',
+      '<cmd>Yazi toggle<cr>',
+      desc = 'Resume the last yazi session',
+    },
+  },
+}
