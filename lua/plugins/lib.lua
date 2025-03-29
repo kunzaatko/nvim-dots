621f58aaf2f7bfd4a0d2d1dbd1030622c4dde031
Martin Kunz <martinkunz@email.cz>
2025-03-29 15:15:49 +0100

    fix(plugins): Remove unused plugin specs

---
+++ lua/plugins/lib.lua
@@ -0,0 +1,11 @@
+return {
+  { -- abolish - used for abbreviations in ftplugin
+    'Mateiadrielrafael/scrap.nvim',
+    name = 'scrap',
+    ft = { 'tex', 'markdown' },
+  },
+  {
+    'nvim-lua/plenary.nvim',
+    name = 'plenary',
+  },
+}
