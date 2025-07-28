134aadabbbe0dceb113d237c7a07504ac7d7342b
Martin Kunz <martinkunz@email.cz>
2025-07-28 11:20:47 +0200

    Revert "fix(keymap): Removed `Y` keymap"
    This reverts commit eb6011968c6051ba7b6cff1d0c11feeecee693e2.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -132,6 +132,12 @@ vim.keymap.set({ 'n', 'x' }, 'y', function()
   return 'y'
 end, { expr = true })
 
+-- HACK: Default `Y` mapping does not work with `TextYankPost` <28-07-25>
+vim.keymap.set('n', 'Y', function()
+  cursorPreYank = vim.api.nvim_win_get_cursor(0)
+  return 'y$'
+end, { expr = true, desc = 'yank to eol' }) -- Y to act as D and C
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     if vim.v.event.operator == 'y' and cursorPreYank then
