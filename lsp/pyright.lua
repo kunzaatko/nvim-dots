650086a3d320513f2022be1a04bf945683e7d971
Martin Kunz <martinkunz@email.cz>
2025-09-02 11:45:27 +0200

    fix: Add better pyright configuration

--- lsp/pyright.lua
+++ lsp/pyright.lua
@@ -1,3 +1,24 @@
+---@brief
+---
+--- https://github.com/microsoft/pyright
+---
+--- `pyright`, a static type checker and language server for python
+
+local function set_python_path(path)
+  local clients = vim.lsp.get_clients {
+    bufnr = vim.api.nvim_get_current_buf(),
+    name = 'pyright',
+  }
+  for _, client in ipairs(clients) do
+    if client.settings then
+      client.settings.python = vim.tbl_deep_extend('force', client.settings.python, { pythonPath = path })
+    else
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = path } })
+    end
+    client:notify('workspace/didChangeConfiguration', { settings = nil })
+  end
+end
+
 local root_files = {
   'pyproject.toml',
   'setup.py',
@@ -8,7 +29,7 @@ local root_files = {
 }
 
 ---@diagnostic disable-next-line: undefined-doc-name
----@type  vim.lsp.ClientConfig
+---@type  vim.lsp.Config
 local opts = {
   cmd = { 'pyright-langserver', '--stdio' },
   filetypes = { 'python' },
@@ -24,6 +45,21 @@ local opts = {
     },
   },
   single_file_support = true,
+  on_attach = function(client, bufnr)
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspPyrightOrganizeImports', function()
+      client:exec_cmd {
+        command = 'pyright.organizeimports',
+        arguments = { vim.uri_from_bufnr(bufnr) },
+      }
+    end, {
+      desc = 'Organize Imports',
+    })
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspPyrightSetPythonPath', set_python_path, {
+      desc = 'Reconfigure pyright with the provided python path',
+      nargs = 1,
+      complete = 'file',
+    })
+  end,
 }
 
 return opts
